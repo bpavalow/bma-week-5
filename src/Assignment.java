9ffb5f4ca75ad1bf2db48f3868525226f5bf93ec
bpavalow <brendan.pavalow@gmail.com>
2012-10-07 12:12:20 -0400

    committing homework assignment

--- src/Assignment.java
+++ src/Assignment.java
@@ -1,3 +1,4 @@
+import java.util.Date;
 
 public class Assignment {
 
@@ -5,8 +6,42 @@ public class Assignment {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Task noArgTask = new Task();
+		Task singleArgTask = new Task("Single Argument Task", new Date(), true, true);
+		
+		System.out.println(noArgTask);
+		System.out.println(singleArgTask);
 	}
-
+	
+	public static String getElement(String[] values, int index) {
+		if (index >= values.length)
+			return null;
+		
+		return values[index];
+	}
+}
+class Task {
+	String name;
+	Date date;
+	boolean completed;
+	boolean recurring;
+	
+	public Task(String name, Date date, boolean completed, boolean recurring) {
+		this.name = name;
+		this.date = date;
+		this.completed = completed;
+		this.recurring = recurring;
+	}
+	
+	public Task() {
+		this.name = "No Argument Task";
+		this.date = new Date();
+		this.completed = false;
+		this.recurring = false;
+	}
+	
+	public String toString() {
+		return "Task, " + this.name + ", was " + ((completed) ? "" : "not ") + "completed on " + date.toString();
+	}
+	
 }
